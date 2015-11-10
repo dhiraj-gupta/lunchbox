ac5c4a98dcfa10c9306ec54e4a83e2af0221ab6d
Nate Swart <nateswart@gmail.com>
2015-11-10 16:04:53 -0500

    Allow settings to control file sync type

--- drupalvm.js
+++ drupalvm.js
@@ -92,6 +92,18 @@ $("#menu_drupalvm_provision").click(function() {
   controlVM(DRUPALVM_PROVISION);
 });
 
+$("#drupalvm_settings_filesync_default").click(function() {
+  saveFileSyncType("");
+})
+
+$("#drupalvm_settings_filesync_rsync").click(function() {
+  saveFileSyncType("rsync");
+})
+
+$("#drupalvm_settings_filesync_nfs").click(function() {
+  saveFileSyncType("nfs");
+})
+
 
 // ------ Event Handlers ------ //
 
@@ -191,7 +203,8 @@ function controlVM(action) {
 
     case DRUPALVM_PROVISION:
       cmd += ' provision ';
-      title = 'Re-provisinging VM';
+      title = 'Re-provisioning VM';
+      drupalvm_needsprovision = false;
       break;
   }
 
@@ -245,6 +258,9 @@ function updateVMStatus() {
         $('#menu_drupalvm_provision').removeClass('disabled');
 
         drupalvm_running = true;
+      }
+      if(!drupalvm_needsprovision) {
+        $("#controlLabel").text("");
       }
 
       $('#drupalvmProcessing').modal('hide');
@@ -346,12 +362,49 @@ function renderSitesRow(servername) {
 function drupalvmBuildSettings() {
   drupalvmHidePanels();
 
-  var installed_extras = drupalvm_config.installed_extras;
+  // VM file sync mechanism
+  var file_sync_type = drupalvm_config.vagrant_synced_folders[0].type;
+  switch(file_sync_type) {
+    case "rsync":
+      $("#drupalvm_settings_filesync_rsync").button('toggle');
+      break;
 
+    case "nfs":
+      $("#drupalvm_settings_filesync_nfs").button('toggle');
+      break;
+
+    default:
+      $("#drupalvm_settings_filesync_default").button('toggle');
+  }
+
+  // Installed extras
+  $('#drupalvm_settings_installedextras input').prop('checked', false); // reset
+  var installed_extras = drupalvm_config.installed_extras;
   for(x in installed_extras) {
     var extra = installed_extras[x];
     $('#drupalvm_settings_' + extra).prop('checked', true);
   }
 
   $("#panel_drupalvm_settings").fadeIn();
+}
+
+function saveFileSyncType(file_sync_type) {
+
+  // Only update if the value is actually different.
+  if(file_sync_type != drupalvm_config.vagrant_synced_folders[0].type) {
+    drupalvm_config.vagrant_synced_folders[0].type = file_sync_type;
+    saveConfigFile();
+  }
+}
+
+function saveConfigFile() {
+  yamlString = YAML.stringify(drupalvm_config, 2);
+  var fs = require('fs');
+  fs.writeFile(drupalvm_home + '/config.yml', yamlString, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+  });
+  $("#controlLabel").text("Needs re-provisioning");
+  drupalvm_needsprovision = true;
 }
