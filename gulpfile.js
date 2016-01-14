061c5d2952d059bc4d72922b63b09b58aa977f63
Yuriy Babenko <yuriyb@gmail.com>
2016-01-13 17:34:21 -0800

    added support for runtime compilation of .less files via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,17 @@ var gulp        = require('gulp'),
   childProcess  = require('child_process'), 
   electron      = require('electron-prebuilt');
 
+var less = require('gulp-less');
+var path = require('path');
+
 // create the gulp task
 gulp.task('default', function () { 
+  // generate .css files from .less files
+  gulp.src('./app/css/*.less').pipe(less({
+    paths: [ path.join(__dirname, 'less', 'includes') ]
+  }))
+  .pipe(gulp.dest('./app/css'));
+
+  // spawn electron
   childProcess.spawn(electron, ['.'], { stdio: 'inherit' }); 
 });
